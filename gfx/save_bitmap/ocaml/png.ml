b82b80dd25264588448f99f5f35fb5dbdb6cb1df
jjhellst <jjhellst@60e57fd4-d74e-0410-81ba-8d1bba1f1622>
2008-09-20 16:46:50 +0000

    Fix bug in Adler accumulation - saved PNGs now open without complaints in GIMP and other image programs

--- gfx/save_bitmap/ocaml/png.ml
+++ gfx/save_bitmap/ocaml/png.ml
@@ -24,9 +24,9 @@ let crc_table =
 let acc_crc crc v =
   crc_table.(I32.to_int ((crc ^ (I32.of_int v)) & I32.of_int 0xff)) ^ (crc >> 8)
 
-let acc_adler (adler1,adler2) v =
-  (I32.rem (I32.add adler1 (I32.of_int (v land 255))) (I32.of_int 65521),
-   I32.rem (I32.add adler1 adler2) (I32.of_int 65521))
+let acc_adler (a1,a2) v =
+  let a1' = (a1 + v) mod 65521 in
+  (a1', ((a2 + a1') mod 65521))
 
 let write_dword chnl i =
   let output_byte_i32 i = 
@@ -53,10 +53,11 @@ let write_png_chnl chnl pixels w h =
     output_byte chnl b;
     acc_crc crc b in
 
-  let output_adler_byte (a1,a2) v =
+  let output_adler_byte (crc,a1,a2) v =
     let b = v land 255 in
     output_byte chnl b;
-    acc_adler (a1,a2) b in
+    let (a1,a2) = acc_adler (a1,a2) b in
+    (acc_crc crc v,a1,a2) in
 
   let crc = 
     List.fold_left
@@ -72,11 +73,8 @@ let write_png_chnl chnl pixels w h =
       let bytes = [(if (y = h-1) then 1 else 0);
                    blocklen; blocklen lsr 8;
                    lnot blocklen; (lnot blocklen) lsr 8] in
-      let crc = 
-        List.fold_left output_crc_byte crc bytes in
-      let adler = 
-        output_adler_byte (adler1,adler2) 0 in
-      let rec output_row x ((a1,a2) as acc) =
+
+      let rec output_row x acc =
         if x < w then
           let pix = pixels.(x+y*w) in
           let pix_bytes = [pix lsr 16; pix lsr 8; pix; 255] in
@@ -84,19 +82,24 @@ let write_png_chnl chnl pixels w h =
             (List.fold_left (fun acc b -> output_adler_byte acc b) acc pix_bytes)
         else
           acc in
-      let (a1,a2) = (output_row 0 adler) in
-      write_rows (crc,a1,a2) (y+1)
+
+      let crc = 
+        List.fold_left output_crc_byte crc bytes in
+      let adler = 
+        output_adler_byte (crc,adler1,adler2) 0 in
+      write_rows (output_row 0 adler) (y+1)
     else 
       acc in
   
   let (crc,adler1,adler2) = 
-    write_rows ((I32.of_string "0x13e5812d"), I32.one, I32.zero) 0 in
-  let bytes = [adler2 >> 8; adler2; adler1 >> 8; adler1] in
-  let crc = List.fold_left (fun a v -> output_crc_byte a (I32.to_int v)) crc bytes in
+    write_rows ((I32.of_string "0x13e5812d"),1,0) 0 in
+
+  let bytes = [adler2 lsr 8; adler2; adler1 lsr 8; adler1] in
+  let crc = List.fold_left output_crc_byte crc bytes in
   write_dword chnl (I32.lognot crc);
   write_dword chnl (I32.zero);
   Printf.fprintf chnl "IEND\xae\x42\x60\x82"
-    
+
     
 (** Save a Targa (.png) file to file `filename'. *)
 let write_png filename pixels w h =
